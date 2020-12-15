9ab8e956ba9da55f71d5fcea4b0579dfb2e91247
soilerl <1062734409@qq.com>
2020-12-15 17:05:43 +0800

    修复一些错误

--- source/data/service/AsyncGetProjectMergeRequestInformationHelper.py
+++ source/data/service/AsyncGetProjectMergeRequestInformationHelper.py
@@ -4,6 +4,8 @@ import json
 
 from datetime import datetime
 
+import source.utils.ApiUtils as ApiUtils
+
 from source.utils.StringKeyUtils import StringKeyUtils
 from source.data.service.AsyncApiHelper import AsyncApiHelper
 from source.config.configPraser import configPraser
@@ -11,12 +13,13 @@ from source.data.service.GetInformationOfParameter import GetInformationOfParame
 
 class AsyncGetProjectInformationHelper:
 
+    pageIndex = 0
 
     #获取项目一页的MergeRequest的信息
     @staticmethod
-    def getOnePageMergeRequestDataOfProject(repo_id, pageIndex):
+    def getOnePageMergeRequestDataOfProject(projectId):
         loop = asyncio.get_event_loop()
-        task = [asyncio.ensure_future(AsyncGetProjectInformationHelper.preProcess(repo_id))]
+        task = [asyncio.ensure_future(AsyncGetProjectInformationHelper.preProcess(projectId))]
         tasks = asyncio.gather(*task)
         loop.run_until_complete(tasks)
 
@@ -32,32 +35,16 @@ class AsyncGetProjectInformationHelper:
         async with semaphore:
             async with aiohttp.ClientSession() as session:
                 api = AsyncGetProjectInformationHelper.getProjectApi(repo_id)
-                jsonList = await AsyncGetProjectInformationHelper.fetchData(session, api)
+                jsonList = await ApiUtils.fetchData(session, api)
                 print(len(jsonList))
                 for v in jsonList:
                     print(v)
 
-    @staticmethod
-    async def fetchData(session, api):
-        headers = {}
-        headers = AsyncApiHelper.getUserAgentHeaders(headers)
-        headers = AsyncApiHelper.getPrivateTokensHeaders(headers)
-        proxy = await AsyncApiHelper.getProxy()
-        try:
-            async with session.get(api, ssl=False, proxy=proxy
-                    , headers=headers, timeout=configPraser.getTimeout()) as response:
-                print("response status: ", response.status)
-                if response.status == 403:
-                    raise 403
-                return await response.json()
-        except Exception as e:
-            print(e)
-            return await AsyncGetProjectInformationHelper.fetchData(session, api)
 
     @staticmethod
-    def getProjectApi(repo_id, pageIndex):
+    def getProjectApi(repo_id):
         api = StringKeyUtils.API_GITLAB + StringKeyUtils.API_GITLAB_MERGE_REQUESTS + \
-              "?scope=all&state=all&page=" + pageIndex
+              "?scope=all&state=all&page=" + str(AsyncGetProjectInformationHelper.pageIndex)
         api = api.replace(StringKeyUtils.STR_GITLAB_REPO_ID, repo_id)
         return api
 
@@ -65,4 +52,6 @@ if __name__ == '__main__':
     getParameterHelper = GetInformationOfParameterHelper("https://gitlab.com/tezos/tezos")
     projectID = getParameterHelper.getProjectID()
     pages = getParameterHelper.getMergeRequestPages()
-    AsyncGetProjectInformationHelper.getOnePageMergeRequestDataOfProject(projectID, pageIndex)
+    for i in range(1, 3):
+        AsyncGetProjectInformationHelper.pageIndex = i
+        AsyncGetProjectInformationHelper.getOnePageMergeRequestDataOfProject(projectID)
--- source/data/service/GetInformationOfParameter.py
+++ source/data/service/GetInformationOfParameter.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from source.utils.StringKeyUtils import StringKeyUtils
 
 """爬虫爬取一些参数"""
 
@@ -16,21 +17,21 @@ class GetInformationOfParameterHelper:
     #初始化头
     def initHeader(self):
         self.header = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
+            StringKeyUtils.STR_HEADER_USER_AGENT: StringKeyUtils.STR_HEADER_USER_AGENT_SET
         }
     #获取HTML内容，返回字符串
-    def getHTML(self) ->str:
+    def getHTML(self, url) ->str:
         try:
-            r = requests.get(self.url, headers=self.header, timeout=30)
+            r = requests.get(url, headers=self.header, timeout=30)
             r.raise_for_status()
             r.encoding = r.apparent_encoding
             return r.text
-        except:
-            print("爬取失败")
+        except Exception as e:
+            print("爬取失败, Exception: ", e)
     #在html网页中提取ProjectID
     def analyzeProjectID(self, soup=BeautifulSoup) -> str:
         strTemp = []
-        for span in soup.find_all('span', attrs={'data-qa-selector':'project_id_content'}):
+        for span in soup.find_all('span', attrs=StringKeyUtils.DIC_ANALYZE_PROJECT_ID_ATTR):
             strTemp.append(span.string)
         if len(strTemp) == 1:
             #allStr格式为 Project ID: 3836952
@@ -40,12 +41,33 @@ class GetInformationOfParameterHelper:
 
     #对外暴露的接口，获取projectID
     def getProjectID(self) -> str:
-        htmlText = helper.getHTML()
+        htmlText = self.getHTML(self.url)
         soup = BeautifulSoup(htmlText, 'lxml')
         projectID = self.analyzeProjectID(soup)
         return projectID
 
-if __name__ == '__main__':
-    helper = GetInformationOfParameterHelper("https://gitlab.com/tezos/tezos")
-    print(helpe r.getProjectID())
+    #对外暴露的接口，获取项目mergeRequest页数
+    def getMergeRequestPages(self) -> int:
+        getPagesUrl = self.url
+        if getPagesUrl[-1] != "/":
+            getPagesUrl = getPagesUrl + "/"
+        getPagesUrl += "-/merge_requests?scope=all&state=all"
+        htmlText = self.getHTML(getPagesUrl)
+        pages = self.analyzePages(htmlText)
+        return pages
 
+    #从html页面中提取page数
+    def analyzePages(self, htmlText='') -> int:
+        soup = BeautifulSoup(htmlText, 'lxml')
+        pageIndexList = []
+        for li in soup.find_all("li", attrs={'class': 'js-pagination-page'}):
+            pageIndexList.append(int(li.findChild().text))
+        pageIndexList.sort(reverse=True)
+        return pageIndexList[0]
+
+
+
+# if __name__ == '__main__':
+#     helper = GetInformationOfParameterHelper("https://gitlab.com/tezos/tezos")
+#     # print(helper.getProjectID())
+#     pages = helper.getMergeRequestPages()
