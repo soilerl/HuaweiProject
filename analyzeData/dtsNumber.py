7a90a707f91274eee7584d7c651252ec1959f44d
soilerl <33306882+soilerl@users.noreply.github.com>
2021-01-08 18:04:41 +0800

    完善问题反馈指标

--- analyzeData/dtsNumber.py
+++ analyzeData/dtsNumber.py
@@ -20,7 +20,8 @@ def connectMongoDB(collectionName=''):
 def analyzeData(serviceName='', date=()) -> []:
     res = []
     dTSVoCollection = connectMongoDB("dTSVo")
-    dtsDict = dTSVoCollection.find({"sbaseline" : re.compile(serviceName)})
+    serviceName = serviceName.strip()
+    dtsDict = dTSVoCollection.find({"sbaseline" : re.compile(serviceName.strip())})
 
     yearMin = date[0]
     monthMin = date[1]
@@ -70,15 +71,23 @@ def calDtsNumber(serviceName='', date=(), dtsNumberDf=DataFrame):
     return dtsNumberDf
 
 #计算问题反馈率（问题单数/mr数）
-def calDtsRate(projectName='', date=()):
-    dtsDic = calDtsNumberDic(projectName, date)
-    mrDic = getMrCountDic(serviceName, date)
+def calDtsRate(projectName='', date=(), df=DataFrame):
+    dtsDic = calDtsNumberDic(projectName, date=date)
+    mrDic = getMrCountDic(projectName, date)
     resDic = {}
-    for k, v in dtsDic:
-        temp = dtsDic[k] / mrDic[k]
+    for k, v in dtsDic.items():
+        if k == 'project':
+            continue
+        mrCount = 1
+        if k in mrDic.keys():
+            mrCount = mrDic[k]
+        temp = int(dtsDic[k]) / int(mrCount)
         resDic[k] = temp
-    return dtsDic
+    resDic["project"] = projectName
+    df = df.append(resDic, ignore_index=True)
+    return df
 
+#计算mr数量
 def getMrCountDic(projectName='', date=()):
     mrList = common.getMergeRequestInstances(projectName)
     resDict = {}
@@ -86,8 +95,11 @@ def getMrCountDic(projectName='', date=()):
         createdAt = mr.created_at
         time = common.tranformStrToDateTime(createdAt)
         if common.checkTime(time, date):
+            monthStr = ''
             if time.month < 10:
                 monthStr = '0' + str(time.month)
+            else:
+                monthStr = str(time.month)
             timeStr = str(time.year) + "-" + monthStr
             if timeStr in resDict.keys():
                 resDict[timeStr] += 1
@@ -99,25 +111,95 @@ def getMrCountDic(projectName='', date=()):
 def getServiceNameList(excelName, sheetName):
     excel = xlrd.open_workbook(excelName)
     sheet = excel.sheet_by_name(sheetName)
-    serviceNameList = sheet.col_values(0)[1:]
+    rows = sheet.nrows
+    serviceNameList = sheet.col_values(0, 1)
     dic = {}
     for serviceName in serviceNameList:
         dic[serviceName] = 1
     return list(dic.keys())
 
+def getSpnameNameList(excelName, sheetName):
+    excel = xlrd.open_workbook(excelName)
+    sheet = excel.sheet_by_name(sheetName)
+    rows = sheet.nrows
+    serviceNameList = sheet.col_values(2, 1)
+    dic = {}
+    for serviceName in serviceNameList:
+        dic[serviceName] = 1
+    return list(dic.keys())
+
+#计算一个项目的所有问题单之和
+def calAllDtsOfProject(serviceNameList, date=()):
+    dtsNumberDicList = []
+    for serviceName in serviceNameList:
+        print(serviceName)
+        dtsNumberDic = calDtsNumberDic(serviceName, date)
+        dtsNumberDicList.append(dtsNumberDic)
+    sumDic = {}
+    for dtsDic in dtsNumberDicList:
+        for k, v in dtsDic.items():
+            if k == 'project':
+                continue
+            if k in sumDic:
+                sumDic[k] += v
+            else:
+                sumDic[k] = v
+    return sumDic
+#计算一个项目的所有mr和
+def calAllMrsOfProject(serviceNameList, date=()):
+    mrDicList = []
+    for serviceName in serviceNameList:
+        print(serviceName)
+        mrDic = getMrCountDic(serviceName, date)
+        mrDicList.append(mrDic)
+    sumDic = {}
+    for mrDic in mrDicList:
+        for k, v in mrDic.items():
+            if k == 'project':
+                continue
+            if k in sumDic:
+                sumDic[k] += v
+            else:
+                sumDic[k] = v
+    return sumDic
 
 if __name__ == '__main__':
+    #云平台
+    serviceNameList = ["WiseCloudIMService", "WiseCloudHOTAService", "WiseCloudMediaHostingService",
+                       "WiseCloudSNService", "WiseCloudContentCommunityService"]
 
-    # serviceNameList = ["WiseCloudIMService", "WiseCloudHOTAService", "WiseCloudMediaHostingService",
-    #                    "WiseCloudSNService", "WiseCloudContentCommunityService"]
+    #pps
+    # serviceNameList = getServiceNameList('PPS9个服务去重整理.xlsx', 'de_duplicates')
+    # spnameList = getSpnameNameList('PPS9个服务去重整理.xlsx', 'de_duplicates')
 
+    # fileName = "dtsNumber.xls"
+    # sheetName = "dtsNumber"
+    # df = createDf((2019, 9, 2020, 11))
+    # for serviceName in serviceNameList:
+    #     df = calDtsNumber(serviceName, (2019, 9, 2020, 11), df)
+    # ExcelHelper().writeDataFrameToExcel(fileName, sheetName, df)
+    # print(df)
 
-    serviceNameList = getServiceNameList('PPS9个服务去重整理.xlsx', 'de_duplicates')
-    fileName = "dtsNumber.xls"
-    sheetName = "dtsNumber"
+    fileName = "dtsRate.xls"
+    sheetName = "dtsRate"
     df = createDf((2019, 9, 2020, 11))
-    for serviceName in serviceNameList:
-        df = calDtsNumber(serviceName, (2019, 9, 2020, 11), df)
+    #PPS
+    # dtsSumDic = calAllDtsOfProject(serviceNameList, (2019, 9, 2020, 11))
+    # mrSumDic = calAllMrsOfProject(spnameList, (2019, 9, 2020, 11))
+    #云平台
+    dtsSumDic = calAllDtsOfProject(serviceNameList, (2019, 9, 2020, 11))
+    mrSumDic = calAllMrsOfProject(serviceNameList, (2019, 9, 2020, 11))
+    resDic = {}
+    for k in dtsSumDic.keys():
+        if k == 'project':
+            continue
+        mrCount = 1
+        if k in mrSumDic.keys():
+            mrCount = mrSumDic[k]
+        temp = int(dtsSumDic[k]) / int(mrCount)
+        resDic[k] = temp
+    resDic["project"] = "wiseContent"
+    df = df.append(resDic, ignore_index=True)
     ExcelHelper().writeDataFrameToExcel(fileName, sheetName, df)
     print(df)
 
